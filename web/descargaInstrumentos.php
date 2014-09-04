cfd6709720542c6ca6561fdaeefb39b59ae5df90
pablozur <jp.ruzc@gmail.com>
2014-09-04 12:37:22 -0400

    - Incorporado texto de error en un  item de la prueba # 1 - Incorporado div cajaCentralDown para que se vean cerradas las cajas

--- web/descargaInstrumentos.php
+++ web/descargaInstrumentos.php
@@ -11,26 +11,26 @@ $rbdColegio = getRbdUsuario($idUsuario);
 <body>
 <div id="principal">
 <?php 
-    require("topMenu.php"); 
-    $navegacion = "Home*curso.php?idCurso=$idCurso,Evaluacion*evaluacionHome.php,Descarga de Instrumentos*#";
-    require("_navegacion.php");
+	require("topMenu.php"); 
+	$navegacion = "Home*curso.php?idCurso=$idCurso,Evaluacion*evaluacionHome.php,Descarga de Instrumentos*#";
+	require("_navegacion.php");
 
 
 ?>
-    
-    <div id="lateralIzq">
-        <?php require("menuleft.php");  ?>
+	
+	<div id="lateralIzq">
+   		<?php require("menuleft.php");	?>
     </div> <!--lateralIzq-->
     
     <div id="lateralDer">
-        <?php require("menuright.php"); ?>
+		<?php require("menuright.php"); ?>
     </div><!--lateralDer-->
     
-    <div id="columnaCentro">
-    <p class="titulo_curso">Evaluaciones de proceso</p>
+	<div id="columnaCentro">
+	<p class="titulo_curso">Evaluaciones de proceso</p>
     <hr /><br />
-    <p class="textoBienvenida">  AtenciÃ³n: error en ÃŒtem 20 de la prueba de 3Âº bÃ¡sico, por favor revisar mÃ¡s detalles en Home.</p>
-    <?php $grupos = getGruposEvaluaciones(); ?>
+    <p class="textoBienvenida">  Atención: error en ítem 20 de la prueba de 3° básico, por favor revisar más detalles en Home.</p>
+	<?php $grupos = getGruposEvaluaciones(); ?>
     <?php foreach ($grupos as $grupo) { ?>
     <?php $recursos = getEvaluacionGrupo($grupo["idGrupoEvaluacion"], $idUsuario); ?>
     <?php if(count($recursos[0]) >0 || count($recursos[1]) >0 || count($recursos[2]) >0){ ?>
@@ -83,8 +83,8 @@ $rbdColegio = getRbdUsuario($idUsuario);
         </div>
         <br>
     <?php }} ?>
-        
-        <br>
+		
+		<br>
         <center>
             <button class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" onClick="window.open('evaluacionHome.php','_self')">
                 <span class="ui-button-text">Volver</span>
@@ -92,9 +92,9 @@ $rbdColegio = getRbdUsuario($idUsuario);
         </center>
     </div> <!--columnaCentro-->
 
-    <?php 
-        
-        require("pie.php");
+	<?php 
+    	
+		require("pie.php");
     ?> 
  
 </div><!--principal--> 
