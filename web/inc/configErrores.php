e722d5a3e8be018360202aa5361e22930b7c031c
pablozur <jp.ruzc@gmail.com>
2014-09-04 16:24:22 -0400

    corregidas las ip para debuguear

--- web/inc/configErrores.php
+++ web/inc/configErrores.php
@@ -1,14 +1,16 @@
 <?php
 
-$ipJotape = "158.170.118.22";
-$ipPelon = "158.170.118.5";
-$ipJorge = "158.170.119.2";
+$debug_on = false;
+
+$ip1 = "158.170.118.11";
+$ip2 = "158.170.119.78"; //juanjo
 
 
 if(
-   		$_SERVER['REMOTE_ADDR']== $ipJotape
-   ||	$_SERVER['REMOTE_ADDR']== $ipPelon 
-   ||	$_SERVER['REMOTE_ADDR']== $ipJorge 
+   		$_SERVER['REMOTE_ADDR']== $ip1
+   ||	$_SERVER['REMOTE_ADDR']== $ip2 
+   
+   && $debug_on
    )
 {
   ini_set('display_errors','On');
