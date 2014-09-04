3b18a17af53cc1ab0e8f283c66d5aa775ba40d88
pablozur <jp.ruzc@gmail.com>
2014-09-04 13:53:57 -0400

    Actualizada a la versión más reciente del index

--- web/index.php
+++ web/index.php
@@ -29,11 +29,11 @@ $(function() {
         <p class="titulo_div" style="width: 180px;">Noticias</p>
         <p class="info_div" style="width: 180px;" align="justify">
         	
-            <a href="http://asiapacifico.bcn.cl/noticias/cultura-y-sociedad/dinko-mitrovich-centro-klein-balance-metodo-singapur-en-chile" target="_blank">Asesor√≠a Singapur en la Prensa:  "El m√©todo Singapur se est√° haciendo muy popular en Chile"</a>
+            <a href="http://asiapacifico.bcn.cl/noticias/cultura-y-sociedad/dinko-mitrovich-centro-klein-balance-metodo-singapur-en-chile" target="_blank">AsesorÌa Singapur en la Prensa:  "El mÈtodo Singapur se est· haciendo muy popular en Chile"</a>
             <br /><br />
-            Desde el a√±o 2009 el Centro F√©lix Klein de la Universidad de Santiago de Chile adapta los textos escolares 
-            "Pensar sin L√≠mites: Matem√°tica M√©todo de Singapur" de la editorial Marshall Cavendish para Chile y Latinoam√©rica, 
-            y actualmente se encuentran realizando el sexto libro, que completa la serie y que saldr√° al sistema educacional 
+            Desde el aÒo 2009 el Centro FÈlix Klein de la Universidad de Santiago de Chile adapta los textos escolares 
+            "Pensar sin LÌmites: Matem·tica MÈtodo de Singapur" de la editorial Marshall Cavendish para Chile y LatinoamÈrica, 
+            y actualmente se encuentran realizando el sexto libro, que completa la serie y que saldr· al sistema educacional 
             chileno el 2014
         </p>
     
@@ -64,120 +64,21 @@ $(function() {
   </div>
     
      <div id="columnaCentro" style="padding: 14px; width: 540px;"><p align="justify">     
-       <p align="justify">     
+       <p align="justify">       
        <p align="justify"><br />
-       Bienvenidas y bienvenidos a la Plataforma Virtual de los cursos de capacitaci√≥n y asesor√≠a en la propuesta did√°ctica para la¬† Ense√±anza de las Matem√°ticas de Singapur. <br />
+		Bienvenidas y bienvenidos a la Plataforma Virtual de los cursos de capacitaciÛn y asesorÌa en la propuesta did·ctica para la† EnseÒanza de las Matem·ticas de Singapur. <br />
         <br />
-        El Centro Felix Klein, de Investigaci√≥n, Experimentaci√≥n y transferencia en Did√°ctica de las Matem√°ticas y las Ciencias perteneciente a la Facultad de Ciencia de la Universidad de Santiago de Chile, ha sido el encargado de adaptar los textos ¬ìMy Pals Are Here¬î de Singapur al sistema educativo nacional, con lo que hemos podido concretar la versi√≥n en espa√±ol titulada "Pensar sin L√≠mites: Matem√°tica M√©todo Singapur". <br />
+        El Centro Felix Klein, de InvestigaciÛn, ExperimentaciÛn y transferencia en Did·ctica de las Matem·ticas y las Ciencias perteneciente a la Facultad de Ciencia de la Universidad de Santiago de Chile, ha sido el encargado de adaptar los textos ìMy Pals Are Hereî de Singapur al sistema educativo nacional, con lo que hemos podido concretar la versiÛn en espaÒol titulada "Pensar sin LÌmites: Matem·tica MÈtodo Singapur". <br />
         <br />
-        Este a√±o 2014 se llevar√°n a cabo cursos de formaci√≥n docente para los niveles de 1¬∫ a 5¬∫ b√°sico y para los directivos t√©cnicos de los establecimientos educacionales que han optado por este programa. Estos cursos contemplan una actualizaci√≥n de conocimientos matem√°ticos y did√°cticos coherentes con la propuesta plasmada en los textos de la serie ¬ìPensar sin L√≠mites¬î, centr√°ndose en la gesti√≥n de aula, a trav√©s de la preparaci√≥n, an√°lisis y reflexi√≥n de procesos de ense√±anza aprendizaje matem√°ticos. 
-
-<!--        Bienvenidas y bienvenidos a la
-  
-
-         Plataforma Virtual de los cursos de capacitaci&oacute;n y asesor&iacute;a
-         en la propuesta did&aacute;ctica para la&nbsp; ense&ntilde;anza de las
-         matem&aacute;ticas de Singapur. <br />
-         <br />
-         El Centro Felix Klein, de Investigaci&oacute;n, Experimentaci&oacute;n
-         y transferencia en Did&aacute;ctica de las Matem&aacute;ticas y las
-         Ciencias perteneciente a la Facultad de Ciencia de la Universidad de
-         Santiago de Chile, ha sido el encargado de adaptar los textos &ldquo;My
-         Pals Are Here&rdquo;de Singapur al sistema educativo nacional, con lo
-         que hemos podido concretar la versi&oacute;n en espa&ntilde;ol titulada &quot;<strong>Pensar
-         sin L&iacute;mites: Matem&aacute;tica M&eacute;todo Singapur</strong>&quot;.<br />
-         <br />
-         Este a&ntilde;o 2013 se llevar&aacute;n a cabo cursos de formaci&oacute;n
-         docente para los niveles de 1&ordm; a 5&ordm; b&aacute;sico y para los
-         directivos t&eacute;cnicos de los establecimientos educacionales que
-         han optado por este programa. Estos cursos contemplan una actualizaci&oacute;n
-         de conocimientos matem&aacute;ticos y did&aacute;cticos coherentes con
-         la propuesta plasmada en los textos de la serie &ldquo;Pensar sin L&iacute;mites&rdquo;,
-         centr&aacute;ndose en la gesti&oacute;n de aula, a trav&eacute;s de
-         la preparaci&oacute;n, an&aacute;lisis y reflexi&oacute;n de procesos
-         de ense&ntilde;anza aprendizaje matem&aacute;ticos.-->
-
-        
-     	<!--  <img src="img/avisoReceso.jpg" width="555">-->
+        Este aÒo 2014 se llevar·n a cabo cursos de formaciÛn docente para los niveles de 1∫ a 5∫ b·sico y para los directivos tÈcnicos de los establecimientos educacionales que han optado por este programa. Estos cursos contemplan una actualizaciÛn de conocimientos matem·ticos y did·cticos coherentes con la propuesta plasmada en los textos de la serie ìPensar sin LÌmitesî, centr·ndose en la gestiÛn de aula, a travÈs de la preparaciÛn, an·lisis y reflexiÛn de procesos de enseÒanza aprendizaje matem·ticos. 
+               
+     	
      
   </div> 
   
      
        <?php //  require("misCursos.php");?>
-     
-               <!-- <hr />
-                
-             
-                <a class="button" href="#"><span><div class="guardar">boton</div></span></a><br /><br />
-                <a class="button" href="#"><span><div class="listar">boton</div></span></a><br /><br />
-                <a class="button" href="#"><span><div class="preguntar">boton</div></span></a><br /><br />
-                <a class="button" href="#"><span><div class="precio">boton</div></span></a><br /><br />
-                <a class="button" href="#"><span><div class="pagos">boton</div></span></a><br /><br />
-                <a class="button" href="#"><span><div class="refrescar">boton</div></span></a><br /><br /> 
-
-                <hr />
-                
-    
-                <table id="tabla" class="tablesorter"> 
-                <thead> 
-                    <tr> 
-                    <th>RUT</th> 
-                    <th>Razon Social</th>  
-                    <th>Direcci√≥n</th> 
-                    <th>Regi√≥n</th>
-        
-                    <th>Comuna</th>
-                    <th>Correo</th>  
-                    <th colspan="2">Opciones</th> 
-                    </tr> 
-                </thead> 
-                <tbody> 
-                	<tr valign="top"> 
-                    <td >2-7</td> 
-                    <td >ESPINACA CREATIVO</td>  
-                    <td >WKWMWKLKLW</td> 
-                    <td >Regi√≥n Metropolitana</td> 
-                    <td >MARIA PINTO</td> 
-                    <td >ESPINACA@ESPINACA.CL</td>  		
-                    <td width="50"><a href="prv_editar.php?id=4"><img src="css/btn/editar.gif" border="0"></a></td>
-        
-                    <td width="70"><a href="javascript:eliminar(4);"><img src="css/btn/cancelar.gif" border="0"></a></td> 
-                    </tr>
-                                <tr valign="top"> 
-                    <td >12345676</td> 
-                    <td >dimacofi</td>  
-                    <td >agustinas 786</td> 
-                    <td >Regi√≥n Metropolitana</td> 
-                    <td >SANTIAGO CENTRO</td> 
-                    <td >dimacofi@yahoo.com</td>  		
-                    <td width="50"><a href="prv_editar.php?id=3"><img src="css/btn/editar.gif" border="0"></a></td>
-        
-                    <td width="70"><a href="javascript:eliminar(3);"><img src="css/btn/cancelar.gif" border="0"></a></td> 
-                    </tr>
-                                <tr valign="top"> 
-                    <td >12345678</td> 
-                    <td >todopoleras</td>  
-                    <td >lakjdsbheug87</td> 
-                    <td >Regi√≥n Metropolitana</td> 
-                    <td >PROVIDENCIA</td> 
-                    <td >info@todopoleras.cl</td>  		
-                    <td width="50"><a href="prv_editar.php?id=2"><img src="css/btn/editar.gif" border="0"></a></td>
-        
-                    <td width="70"><a href="javascript:eliminar(2);"><img src="css/btn/cancelar.gif" border="0"></a></td> 
-                    </tr>
-                    </tbody> 
-                </table> 
-                <div id="pager" class="pager">
-                    <form>
-                        <img src="css/tabla/first.png" class="first"/>
-                        <img src="css/tabla/prev.png" class="prev"/>
-                        <input type="text" class="pagedisplay"/>
-                        <img src="css/tabla/next.png" class="next"/>
-            
-                        <img src="css/tabla/last.png" class="last"/>
-                        <input type="hidden" class="pagesize" value="1"><?php  //Registros por paginas  ?> 
-                    </form>
-                </div>-->
+               
 
                 
                 
